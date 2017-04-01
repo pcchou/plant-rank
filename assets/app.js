2e9abffd05d3b60f426090d57fa5a9c05ef415fa
Philip Ping-yu Chou <pcchou@pcchou.me>
2017-04-01 16:49:59 +0800

    Add indicators for sorting methods

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,25 @@
 var state = 0;
+$('#algotext').css('font-weight', 'bold');
 $('h1').on('click', function () {
   switch (state) {
     case 0:
+      $('#algotext').css('font-weight', '');
       $('#algotable').toggle();
+      $('#algocounttext').css('font-weight', 'bold');
       $('#algocounttable').toggle();
       state++;
       break;
     case 1:
-      $('#counttable').toggle();
+      $('#algocounttext').css('font-weight', '');
       $('#algocounttable').toggle();
+      $('#counttext').css('font-weight', 'bold');
+      $('#counttable').toggle();
       state++;
       break;
     case 2:
+      $('#counttext').css('font-weight', '');
       $('#counttable').toggle();
+      $('#algotext').css('font-weight', 'bold');
       $('#algotable').toggle();
       state = 0;
       break;
